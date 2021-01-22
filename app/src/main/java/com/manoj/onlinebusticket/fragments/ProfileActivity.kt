14ed96c58345a664349da30e64e46daeb5a79b14
ManojKC123 <57611035+ManojKC123@users.noreply.github.com>
2021-01-22 21:32:16 +0545

    Creation of Profile Fragment layout

--- app/src/main/java/com/manoj/onlinebusticket/fragments/ProfileActivity.kt
+++ app/src/main/java/com/manoj/onlinebusticket/fragments/ProfileActivity.kt
@@ -7,6 +7,6 @@ import com.manoj.onlinebusticket.R
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.fragment_profile)
     }
 }

1820038bdaa8ed51e9bdc37cf8aa2ad8bacdf91a
ManojKC123 <57611035+ManojKC123@users.noreply.github.com>
2021-01-22 21:16:33 +0545

    initial commit

---
+++ app/src/main/java/com/manoj/onlinebusticket/fragments/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.manoj.onlinebusticket.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manoj.onlinebusticket.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}

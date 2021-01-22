4a00e0086d1e6e4791d2a9321faa4bc5a254e959
ManojKC123 <57611035+ManojKC123@users.noreply.github.com>
2021-01-22 21:53:28 +0545

    Setting up Login Button Listner

--- app/src/main/java/com/manoj/onlinebusticket/LoginActivity.kt
+++ app/src/main/java/com/manoj/onlinebusticket/LoginActivity.kt
@@ -1,11 +1,42 @@
 package com.manoj.onlinebusticket
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        btnLogin.setOnClickListener {
+
+            if (etPassword.text.toString() == "admin" && etUsername.text.toString() == "admin"){
+                val intent = Intent(this@LoginActivity as Context, MainActivity::class.java)
+                this@LoginActivity.startActivity(intent)
+                finish()
+            }
+            else {
+                Toast.makeText(
+                    this@LoginActivity as Context,
+                    "Either Username or Password is Incorrect",
+                    Toast.LENGTH_SHORT
+                ).show()
+                etUsername.error = "Username or Password is incorrect"
+                etUsername.requestFocus()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/manoj/onlinebusticket/fragments/ProfileActivity.kt
+++ app/src/main/java/com/manoj/onlinebusticket/fragments/ProfileActivity.kt
@@ -9,18 +9,12 @@ import com.manoj.onlinebusticket.R
 
 class ProfileActivity : AppCompatActivity() {
 
-    private lateinit var etUsername: EditText
-    private lateinit var etPassword: EditText
-    private lateinit var btnLogin: Button
-    private lateinit var btnSignUp: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_profile)
-        etUsername = findViewById(R.id.EditText_email)
-        etPassword = findViewById(R.id.EditText_password)
-        btnLogin = findViewById(R.id.loginbtn)
-        btnSignUp= findViewById(R.id.signupbtn)
+
 
     }
 }

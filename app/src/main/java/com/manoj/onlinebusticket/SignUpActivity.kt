5f6ce2564dc4a0ab6f3b2942135a1cf5ccde34f3
ManojKC123 <57611035+ManojKC123@users.noreply.github.com>
2021-01-28 21:57:55 +0545

    Binding the elements in Signup Activity

--- app/src/main/java/com/manoj/onlinebusticket/SignUpActivity.kt
+++ app/src/main/java/com/manoj/onlinebusticket/SignUpActivity.kt
@@ -2,10 +2,26 @@ package com.manoj.onlinebusticket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var etname : EditText
+    private lateinit var etmobile : EditText
+    private lateinit var etaddress : EditText
+    private lateinit var etmailid : EditText
+    private lateinit var etpassword : EditText
+    private lateinit var btnSignUp : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        etname = findViewById(R.id.name)
+        etmobile = findViewById(R.id.number)
+        etaddress = findViewById(R.id.address)
+        etmailid = findViewById(R.id.mailId)
+        etpassword = findViewById(R.id.pass)
+        btnSignUp = findViewById(R.id.submit)
+
     }
 }

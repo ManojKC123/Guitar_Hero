f6a9388b9baa298c8d170f86d0f35e61e2e35da6
ManojKC123 <57611035+ManojKC123@users.noreply.github.com>
2021-02-27 21:42:11 +0545

    Product Adapter

--- app/src/main/java/com/manoj/guitarhero/MainActivity.kt
+++ app/src/main/java/com/manoj/guitarhero/MainActivity.kt
@@ -3,16 +3,20 @@ package com.manoj.guitarhero
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.manoj.guitarhero.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnProfileFragment: ImageView
+    private lateinit var product_rview: RecyclerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnProfileFragment = findViewById(R.id.prof)
+        product_rview = findViewById(R.id.product_rview)
 
         btnProfileFragment.setOnClickListener{
             supportFragmentManager.beginTransaction().apply {
@@ -21,7 +25,11 @@ class MainActivity : AppCompatActivity() {
                 commit()
             }
         }
+        product_rview.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ProductAdapater{}
 
 
+        }
     }
 }
---
+++ app/src/main/java/com/manoj/guitarhero/adapter/ProductAdapter.kt
@@ -0,0 +1,23 @@
+package com.manoj.guitarhero.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductAdapter(private val products:ArrayList<Product>): RecyclerView.Adapter<ProductAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ProductAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+    class ViewHolder {
+
+    }
+
+}

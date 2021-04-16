aac07613dbbf38ea8264d52eeb9695e101d24b3d
ManojKC123 <57611035+ManojKC123@users.noreply.github.com>
2021-04-16 20:20:53 +0545

    Guitar Hero Wear

---
+++ guitar_hero_wear/src/main/java/com/manoj/guitar_hero_wear/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.manoj.guitar_hero_wear
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : WearableActivity() {
+    private lateinit var login : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        login = findViewById(R.id.login)
+
+        // Enables Always-on
+        setAmbientEnabled()
+
+        login.setOnClickListener{
+            Toast.makeText(
+                this,
+                "Register Successful",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+    }
+}
